04259aa8e465863cfe08056ddfee424044d53f8b
Josue Igiraneza <igiranezaj28@gmail.com>
2024-05-24 10:44:24 +0200

    Question 3

--- Q3.js
+++ Q3.js
@@ -1,24 +1,21 @@
 const array1 = [
    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-   { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+   { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 const array2 = [
    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-   { id: 2, name: "Bob", details: { age: 25 , city: "Los Angeles" } }
+   { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 const deepEqualArrays = (array1, array2) => {
-
    if (array1.length !== array2.length) return false;
 
-   if(JSON.stringify(array1) === JSON.stringify(array2)) {
-      return true
+   if (JSON.stringify(array1) === JSON.stringify(array2)) {
+      return true;
    } else {
       return false;
    }
+};
 
-}
-
-console.log(deepEqualArrays(array1, array2));
-
+console.log(deepEqualArrays(array1, array2)); //true
